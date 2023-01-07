c0fa55b71d80a8abb6d369c0e97f89b4b077f14b
Shimada666 <649940882@qq.com>
2023-01-07 18:05:04 +0800

    feat: enhance example component

--- src/TemplateComponent.vue
+++ src/TemplateComponent.vue
@@ -1,11 +1,38 @@
 <script lang="ts" setup>
-import { version } from 'vue-demi'
+import { computed, ref, version, watch } from 'vue-demi'
+
+withDefaults(defineProps<{ msg: string; name: string | number }>(), { msg: 'Hello', name: 'my friend' })
+const emit = defineEmits<{
+  (event: 'update', value: number): void
+}>()
+
+const count = ref(0)
+const doubled = computed(() => count.value * 2)
+
+function inc() {
+  count.value += 1
+}
+function dec() {
+  count.value -= 1
+}
+
+const decText = '<b>Dec</b>'
+
+watch(count, value => emit('update', value))
 </script>
 
 <template>
   <div>
     <div class="template-component">
       Template Component. Vue version: {{ version }}
+    </div>
+    <div>
+      <h3>{{ msg }}, {{ name }}</h3>
+      <button @click="inc">
+        Inc
+      </button>
+      <div>{{ count }} x 2 = {{ doubled }}</div>
+      <button @click="dec()" v-html="decText" />
     </div>
   </div>
 </template>
